39492bbc09be6ba56f0576d32289960aa2abef3f
dzezo <dzezonikola@gmail.com>
2019-02-10 19:58:31 +0100

    TextMesh Pro
    1. Ubacen je TMP, trenutno je samo postavljen na panelu O GRADU, i generisana su dva SDF fonta OSC-Light i OSC-LightItalic, nisam video potrebu da radim i za Bold
    2. Ubaceni su dugmici na panelu U RATU i animirani su
    3. Prepravljena je skripta za instanciranje i uklanjanje prefab-a sa scene kako bi obuhvatio i prefab-ove za bitke

---
+++ Assets/Scripts/DisplayPrefab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Skripta ide na dugme koje treba da instancira prefab nakon klika
+public class DisplayPrefab : MonoBehaviour
+{
+    // Prefab koji se instancira se postavlja u inspektoru
+    public GameObject prefab;
+
+    // Panel na kome se instancira prefab
+    private GameObject prefabParent;
+
+    private Button btn;
+
+    // Inicijalizacija
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(InstantiatePrefab);
+        prefabParent = GameObject.FindWithTag("MasterCanvas");
+    }
+
+    // OnClick metoda za instanciranje
+    private void InstantiatePrefab()
+    {
+        Instantiate(prefab, prefabParent.transform, false);
+    }
+}
---
+++ Assets/Scripts/RemoveBattle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Skripta za back dugme kod panela za bitku
+public class RemoveBattle : MonoBehaviour
+{
+    // Panel sa bitkom
+    private GameObject battlePanel;
+
+    // Panel sa bitkama
+    private GameObject battlefieldPanel;
+
+    private Button btn;
+
+    // Inicijalizacija
+    void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(DestroyCitizen);
+        battlePanel = GameObject.FindWithTag("Battle");
+        battlefieldPanel = GameObject.FindWithTag("Battlefield");
+        battlefieldPanel.SetActive(false);
+    }
+
+    // Uklanja bitku sa scene i vraca se na bitke
+    private void DestroyCitizen()
+    {
+        battlefieldPanel.SetActive(true);
+        Destroy(battlePanel);
+    }
+}
